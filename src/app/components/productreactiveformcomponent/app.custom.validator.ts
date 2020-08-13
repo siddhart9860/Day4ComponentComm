11dde92334a7bf110336316cddf641c3ee4d730d
Siddharth Tathe <47735494+stathe9@users.noreply.github.com>
2020-08-13 23:06:25 +0530

    Check-in Day4 assignment

---
+++ src/app/components/productreactiveformcomponent/app.custom.validator.ts
@@ -0,0 +1,38 @@
+import { Logic } from './../../model/logic';
+import { AbstractControl } from '@angular/forms';
+import { Product } from 'src/app/model/app.product.model';
+export class CustomValidator {
+  // if the data is valid then the method returns null
+  // else for invalid value the method return  JSON object e.g.
+  // {even:false} or {odd:true}
+  static CheckEven(control: AbstractControl): any {
+    // tslint:disable-next-line: radix
+    const value = parseInt(control.value);
+    if (value % 2 === 0) {
+      return null; // valid
+    } else {
+      return { even: false }; // invalid
+    }
+  }
+
+  static Duplicate(control: AbstractControl): any {
+    const value = control.value;
+
+    const index = new Logic().getProducts().findIndex(x => x.ProductId === value);
+    if (index > -1) {
+      return { duplicate: true };
+    }
+    return null;
+  }
+
+  static CheckSpace(control: AbstractControl): any {
+    // tslint:disable-next-line: radix
+    const value = control.value;
+
+    if (value.split(' ').length > 1) {
+      return { space: false };
+    }
+
+    return null;
+  }
+}
